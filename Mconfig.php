8c4ac1ba75c0df353d38a3727c3b0e1f28945a49
ohad <ohad@theora.com>
2020-08-26 03:22:40 +0300

    SnapShot

--- Mconfig.php
+++ Mconfig.php
@@ -11,27 +11,14 @@
   *
   * M_DBNAME - the name of the default database
   *
-  * Quick installation instructions:
-  * 1. Uncompress M.tar.gz somwhere under the document root of your server.
-  * 2. Using mysql, create a new database called mdemo: create database mdemo default charset utf8
-  * 3. In the file Mdemo/Mconfig.php, change M_USER and M_PASSWORD to your access credentials.
-  * 4. The installation is complete. Point your browser to Mdemo.
-  *
   * @package M
   * @author Ohad Aloni
   */
 /*------------------------------------------------------------*/
-/**
- * there is no need to change this file at all if these constants
- * are define before including the M files
- * this is useful when several applications with different
- * configurations use the same M source
- */
-
 if ( ! defined('M_USER') )
-	define('M_USER', 'msdb');
+	define('M_USER', 'M_USER not defined');
 if ( ! defined('M_PASSWORD') )
-	define('M_PASSWORD', 'msdb');
+	define('M_PASSWORD', 'M_PASSWORD not defined');
 if ( ! defined('M_DBNAME') )
-	define('M_DBNAME', 'mdemo');
+	define('M_DBNAME', 'M_DBNAME not defined');
 /*------------------------------------------------------------*/

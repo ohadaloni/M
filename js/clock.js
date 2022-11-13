d4b7ba0a5976121dbce6524e4fcc34cc6582fb4c
ohad <ohad@theora.com>
2022-11-13 04:57:27 +0200

    SnapShot

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+function clock() {
+  const today = new Date();
+  Y = today.getFullYear();
+  m = today.getMonth();
+  d = today.getDate();
+  G = today.getHours();
+  i = today.getMinutes();
+  s = today.getSeconds();
+  m++;
+  m = pad0(m);
+  d = pad0(d);
+  i = pad0(i);
+  s = pad0(s);
+  document.getElementById('clock').innerHTML = Y + "-" + m + "-" + d + " " +  G + ":" + i + ":" + s;
+  setTimeout(clock, 1000);
+}
+
+function pad0(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}

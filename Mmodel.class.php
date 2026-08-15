7fdd6f957fd1c7b65a754fc0b26d0158d62adc1d
ohad <ohad@theora.com>
2026-08-15 06:53:44 +0300

    gitDiffs

--- Mmodel.class.php
+++ Mmodel.class.php
@@ -958,7 +958,7 @@ class Mmodel {
 			if ( strncmp($dataType, "int(", 4) == 0 || $dataType == "double" )
 				$value = str_replace(",", "", $value);
 			$str = $value;
-			if ( $str === null )
+			if ( ! $str )
 				continue;
 			$row[$fname] = $str;
 		}
